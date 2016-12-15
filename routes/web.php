654b79e16138a1f1aac53d76135e57a7e20b2fbc
Lai Thuy Ninh <thuyninh.esc@gmail.com>
2016-12-16 04:57:29 +0700

    add admin

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,23 @@ Route::get('/order/{id}/{date_in}/{date_out}/{user_id}', ['as'=>'getOrder', 'use
 Auth::routes();
 
 Route::get('customer/{id}/index', 'CustomerServiceController@index');
+
+Route::get('/admin/checkin', 'Admin\CheckInOutController@checkIn');
+Route::put('/admin/commitcheckin', 'Admin\CheckInOutController@commitCheckIn');
+
+Route::get('/admin/checkout', 'Admin\CheckInOutController@checkOut');
+Route::put('/admin/commitcheckout', 'Admin\CheckInOutController@commitCheckOut');
+Route::post('/getOrderInfo', 'Admin\CheckInOutController@getOrderInfo');
+Route::post('/getCheckoutOrderInfo', 'Admin\CheckInOutController@getCheckoutOrderInfo');
+
+
+Route::get('/admin/orderservicemanager', 'Admin\OrderServiceController@index');
+Route::get('/admin/service', 'Admin\ServiceController@index');
+Route::put('/admin/service/{id}/update', 'Admin\ServiceController@update');
+Route::delete('/admin/service/{id}/delete', 'Admin\ServiceController@delete');
+Route::post('/admin/service/save', 'Admin\ServiceController@save');
+
+
+
+
+
